4d529d28d54001852af89f28dab8ff2fc0062c93
Ana Marija Atanasovska <amatanasovska@yahoo.com>
2024-02-12 20:26:09 +0100

    remove unnecessary comments

--- src/stores/notificationStore.ts
+++ src/stores/notificationStore.ts
@@ -1,24 +1,3 @@
-// import { create } from 'zustand'
-
-// type NotificationStore = {
-//   states: Record<string, any>;
-//   setter: (value: any) => void;
-// };
-
-// export const useNotificationStore = create<NotificationStore>((set) => ({
-//   states: {},
-//   setter: set
-// }));
-
-// export const useNotificationState = (key: string) => {
-//   const { states, setter } = useNotificationStore();
-
-//   return {
-//     state: states[key],
-//     setState: (value: boolean) => setter({ states: { ...states, [key]: value } }),
-//   };
-// };
-
 import { create } from 'zustand';
 
 type NotificationStore = {
